bb71ac5284bee5a321a9c997eae48567b1eab1ac
Ben Ehmke <benehmke@gmail.com>
2020-06-19 14:59:38 -0500

    add offset, outerHeight, and outerWidth

--- index.js
+++ index.js
@@ -59,3 +59,27 @@ export function createElement(tagName = 'div', options={}) {
   });
   return el
 }
+
+export function offset(el){
+    var rect = el.getBoundingClientRect();
+    return { 
+      top: rect.top,
+      left: rect.left
+    }
+}
+
+export function outerHeight(el){
+    var height = el.offsetHeight;
+    var style = getComputedStyle(el);
+
+    height += parseInt(style.marginTop) + parseInt(style.marginBottom);
+    return height;
+}
+
+export function outerWidth(el){
+    var width = el.offsetWidth;
+    var style = getComputedStyle(el);
+
+    width += parseInt(style.marginLeft) + parseInt(style.marginRight);
+    return width;
+}
