6f916a2ead1af1b3693c30c322cb8de0d24351ca
Ben Ehmke <benehmke@gmail.com>
2021-03-05 16:30:03 -0600

    Add bury test

---
+++ test/bury.js
@@ -0,0 +1,23 @@
+import createElement from '../src/createElement.js';
+import bury from '../src/bury.js';
+import * as assert from 'assert';
+
+describe('bury', function () {
+  it('one level', function () {
+    const test = {a: 1}
+    
+    assert.deepEqual({a: 1, b: 2}, bury(test, 'b', 2))
+  });
+  
+  it('deep', function () {
+    const test = {a: 1}
+    
+    assert.deepEqual({a: 1, b: {c: {d: 2}}}, bury(test, 'b', 'c', 'd', 2))
+  });
+  
+  it('key collision', function () {
+    const test = {a: 1}
+    assert.deepEqual({a: {b: 2}}, bury(test, 'a', 'b', 2))
+  })
+  
+});
