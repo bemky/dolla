07f2429fb9e65c51fc23a3a3e6534e81b3e7edd8
Ben Ehmke <benehmke@gmail.com>
2021-03-05 12:16:23 -0600

    refactor index.js

--- src/index.js
+++ src/index.js
@@ -1,71 +1,24 @@
-import {
-  hasClass,
-  addClass,
-  removeClass,
-  toggleClass,
-  isVisible,
-  isFocus,
-  isEmpty,
-  css,
-  serializeForm,
-  serializeFormToJSON
-} from './attributes';
+export ancestors from './ancestors';
+export bury from './bury';
+export createElement from './createElement';
+export css from './css';
+export filter from './filter';
+export isEmpty from './isEmpty';
+export isFocus from './isFocus';
+export isVisible from './isVisible';
+export map from './map';
+export offset from './offset';
+export offsetToViewport from './offsetToViewport';
+export outerHeight from './outerHeight';
+export outerWidth from './outerWidth';
+export remove from './remove';
+export replaceContents from './replaceContents';
+export serializeForm from './serializeForm';
+export serializeFormToJSON from './serializeFormToJSON';
+export trigger from './trigger';
 
-import {
-  closest,
-  ancestors,
-  filter,
-  map
-} from './query';
-
-import {
-  createElement,
-  remove,
-  replaceContents,
-  HTML_ATTRIBUTES,
-  BOOLEAN_ATTRIBUTES
-} from './manipulation';
-
-import {
-  offset,
-  offsetToViewport,
-  outerHeight,
-  outerWidth
-} from './layout';
-
-import {
-  trigger
-} from './events';
 
 export {
-  hasClass,
-  addClass,
-  removeClass,
-  toggleClass,
-  isVisible,
-  isFocus,
-  isEmpty,
-  css,
-  serializeForm,
-  serializeFormToJSON,
-  
-  closest,
-  ancestors,
-  filter,
-  map,
-  
-  createElement,
-  remove,
-  replaceContents,
   HTML_ATTRIBUTES,
-  BOOLEAN_ATTRIBUTES,
-  
-  offset,
-  offsetToViewport,
-  outerHeight,
-  outerWidth,
-  
-  trigger
-};
-
- 
+  BOOLEAN_ATTRIBUTES
+} from './constants';
