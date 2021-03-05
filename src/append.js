c2aca6d1f564231373a3e2beeeb6711155d184e9
Ben Ehmke <benehmke@gmail.com>
2021-03-05 17:18:12 -0600

    add append helper

---
+++ src/append.js
@@ -0,0 +1,31 @@
+export default function append(el, item, escape) {
+  if (Array.isArray(item)) {
+    item.forEach(i => append(el, i, escape))
+  } else if (escape instanceof Element) {
+    const items = Array.from(arguments).slice(1).filter(x => x instanceof Element)
+    items.forEach(i => append(el, i))
+  } else {
+    if (item instanceof Promise) {
+      const holder = document.createElement('span')
+      el.append(holder)
+      
+      item.then(resolvedItem => {
+        append(holder, resolvedItem, escape)
+        holder.childNodes.forEach(child => {
+          holder.insertAdjacentElement('beforebegin', child)
+        })
+        holder.parentNode.removeChild(holder);
+      })
+    } else if (typeof item == "string") {
+      if (escape) {
+        el.append(item)
+      } else {
+        const container = document.createElement('div')
+        container.innerHTML = item
+        el.append(...container.childNodes)
+      }
+    } else if (item !== null && item !== undefined) {
+      el.append(item);
+    }
+  }
+}
---
+++ test/append.js
@@ -0,0 +1,48 @@
+import createElement from '../src/createElement.js';
+import append from '../src/append.js';
+import * as assert from 'assert';
+
+describe('append', function () {
+  it('single element', function () {
+    const el = createElement('div', {id: 'a'})
+    append(el, createElement('div', {id: 'b'}))
+    assert.equal(el.outerHTML, `<div id="a"><div id="b"></div></div>`)
+  });
+  
+  it('array of elements', function () {
+    const el = createElement('div', {id: 'a'})
+    append(el, [createElement('div', {id: 'b'}), createElement('div', {id: 'c'})])
+    assert.equal(el.outerHTML, `<div id="a"><div id="b"></div><div id="c"></div></div>`)
+  });
+  
+  it('multiple element arguments', function () {
+    const el = createElement('div', {id: 'a'})
+    append(el, createElement('div', {id: 'b'}), createElement('div', {id: 'c'}))
+    assert.equal(el.outerHTML, `<div id="a"><div id="b"></div><div id="c"></div></div>`)
+  });
+  
+  it('escaped html', function () {
+    const el = createElement('div', {id: 'a'})
+    append(el, `<div id="b"></div>`, true)
+    assert.equal(el.outerHTML, `<div id="a">&lt;div id="b"&gt;&lt;/div&gt;</div>`)
+  })
+  
+  it('unescaped html', function () {
+    const el = createElement('div', {id: 'a'})
+    append(el, `<div id="b"></div>`)
+    assert.equal(el.outerHTML, `<div id="a"><div id="b"></div></div>`)
+  })
+  
+  it('promise', async function (done) {
+    const el = createElement('div', {id: 'a'})
+    const promise = new Promise(() => {
+      return `<div id="b"></div>`
+    })
+    await append(el, promise)
+    
+    promise.then(() => {
+      assert.equal(el.outerHTML, `<div id="a"><div id="b"></div></div>`)
+    }).then(done, done)
+  })
+  
+});
