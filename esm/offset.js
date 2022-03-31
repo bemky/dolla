0c690e211838f44101ecbc87013f0fcb27e46e18
Ben Ehmke <benehmke@gmail.com>
2022-03-31 11:32:18 -0500

    update offset function to allow for elements with not offsetParent

--- esm/offset.js
+++ esm/offset.js
@@ -1,8 +1,17 @@
 export default function offset(el) {
+  const parent = el.offsetParent;
   const rect = el.getBoundingClientRect();
-  const parentRect = el.offsetParent.getBoundingClientRect();
-  return {
-    top: rect.top + el.offsetParent.scrollTop - parentRect.top,
-    left: rect.left + el.offsetParent.scrollLeft - parentRect.left
-  };
+
+  if (parent) {
+    const parentRect = el.offsetParent.getBoundingClientRect();
+    return {
+      top: rect.top + el.offsetParent.scrollTop - parentRect.top,
+      left: rect.left + el.offsetParent.scrollLeft - parentRect.left
+    };
+  } else {
+    return {
+      top: rect.top,
+      left: rect.left
+    };
+  }
 }
