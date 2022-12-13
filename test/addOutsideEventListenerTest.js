13fe959bd13073790ea877246071fe972576ec6a
Ben Ehmke <benehmke@gmail.com>
2022-12-13 12:56:29 -0600

    add addOutsideEventListener

---
+++ test/addOutsideEventListenerTest.js
@@ -0,0 +1,8 @@
+import createElement from '../src/createElement';
+import trigger from '../src/trigger';
+import addOutsideEventListener from '../src/addOutsideEventListener';
+import * as assert from 'assert';
+
+describe('addOutsideEventListenerTest', function () {
+    
+});
