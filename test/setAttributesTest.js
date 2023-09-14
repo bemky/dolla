59a389cdabbe1c0f3a0fe22d2a52880c9b3d5988
Ben Ehmke <benehmke@gmail.com>
2023-09-14 10:25:40 -0500

    assignAttributes -> setAttributes

--- test/setAttributesTest.js
+++ test/setAttributesTest.js
@@ -1,13 +1,13 @@
-import {assignAttributes, createElement} from 'dolla';
+import {setAttributes, createElement} from 'dolla';
 import * as assert from 'assert';
 
 describe('createElement', function () {
     it('class', function () {
-        assert.equal(assignAttributes(document.createElement("div"), {class: 'test'}).outerHTML, `<div class="test"></div>`)
+        assert.equal(setAttributes(document.createElement("div"), {class: 'test'}).outerHTML, `<div class="test"></div>`)
     });
   
     it('data', function () {
-        const el = assignAttributes(document.createElement("div"), {
+        const el = setAttributes(document.createElement("div"), {
             data: {
                 test: 11
             }
@@ -16,7 +16,7 @@ describe('createElement', function () {
     });
   
     it('style', function () {
-        const el = assignAttributes(document.createElement("div"), {
+        const el = setAttributes(document.createElement("div"), {
             style: {
                 display: 'none'
             }
@@ -25,27 +25,27 @@ describe('createElement', function () {
     });
   
     it('boolean attribute', function () {
-        const el = assignAttributes(document.createElement('input'), {
+        const el = setAttributes(document.createElement('input'), {
             checked: true,
             type: 'checkbox'
         })
         assert.equal(el.outerHTML, `<input type="checkbox">`);
         assert.equal(el.checked, true)
     
-        const el2 = assignAttributes(document.createElement('input'), {
+        const el2 = setAttributes(document.createElement('input'), {
             checked: false,
             type: 'checkbox'
         })
         assert.equal(el2.outerHTML, `<input type="checkbox">`);
         assert.equal(el2.checked, false)
     
-        const el3 = assignAttributes(document.createElement('textarea'), {
+        const el3 = setAttributes(document.createElement('textarea'), {
             readonly: true,
         })
         assert.equal(el3.outerHTML, `<textarea readonly=""></textarea>`);
         assert.equal(el3.readOnly, true)
     
-        const el4 = assignAttributes(document.createElement('textarea'), {
+        const el4 = setAttributes(document.createElement('textarea'), {
             readonly: false,
         })
         assert.equal(el4.outerHTML, `<textarea></textarea>`);
@@ -54,13 +54,13 @@ describe('createElement', function () {
   
     describe('children', function () {
         it('text', function () {
-            assert.equal(assignAttributes(document.createElement("div"), {
+            assert.equal(setAttributes(document.createElement("div"), {
                 children: 'Hello World'
             }).outerHTML, `<div>Hello World</div>`);
         })
     
         it('element', function () {
-            assert.equal(assignAttributes(document.createElement("div"), {
+            assert.equal(setAttributes(document.createElement("div"), {
                 children: createElement('span', {
                     children: 'Hello World'
                 })
@@ -68,7 +68,7 @@ describe('createElement', function () {
         })
     
         it('null', function () {
-            assert.equal(assignAttributes(document.createElement("div"), {
+            assert.equal(setAttributes(document.createElement("div"), {
                 children: createElement('span', {
                     children: null
                 })
@@ -76,7 +76,7 @@ describe('createElement', function () {
         })
     
         it('undefined', function () {
-            assert.equal(assignAttributes(document.createElement("div"), {
+            assert.equal(setAttributes(document.createElement("div"), {
                 children: createElement('span', {
                     children: undefined
                 })
@@ -84,7 +84,7 @@ describe('createElement', function () {
         })
     
         it('object', function () {
-            assert.equal(assignAttributes(document.createElement("div"), {
+            assert.equal(setAttributes(document.createElement("div"), {
                 children: {
                     tag: 'span',
                     children: 'Hello World'
@@ -93,47 +93,47 @@ describe('createElement', function () {
         })
     
         it('NodeList', function () {
-            const el = assignAttributes(document.createElement("div"), {
+            const el = setAttributes(document.createElement("div"), {
                 children: [{
                     children: 'Hello World'
                 }]
             })
-            assert.equal(assignAttributes(document.createElement("div"), {
+            assert.equal(setAttributes(document.createElement("div"), {
                 children: el.childNodes
             }).outerHTML, `<div><div>Hello World</div></div>`);
         })
     
         it('HTMLCollection', function () {
-            const el = assignAttributes(document.createElement("div"), {
+            const el = setAttributes(document.createElement("div"), {
                 children: [{
                     children: 'Hello World'
                 }]
             })
-            assert.equal(assignAttributes(document.createElement("div"), {
+            assert.equal(setAttributes(document.createElement("div"), {
                 children: el.children
             }).outerHTML, `<div><div>Hello World</div></div>`);
         })
     
         it('array with text', function () {
-            assert.equal(assignAttributes(document.createElement("div"), {
+            assert.equal(setAttributes(document.createElement("div"), {
                 children: ['Direct HTML', 'Hello World']
             }).outerHTML, `<div>Direct HTMLHello World</div>`);
         })
     
         it('array with null', function () {
-            assert.equal(assignAttributes(document.createElement("div"), {
+            assert.equal(setAttributes(document.createElement("div"), {
                 children: ['Direct HTML', null]
             }).outerHTML, `<div>Direct HTML</div>`);
         })
     
         it('array with undefined', function () {
-            assert.equal(assignAttributes(document.createElement("div"), {
+            assert.equal(setAttributes(document.createElement("div"), {
                 children: ['Direct HTML', undefined]
             }).outerHTML, `<div>Direct HTML</div>`);
         })
     
         it('array with object', function () {
-            assert.equal(assignAttributes(document.createElement("div"), {
+            assert.equal(setAttributes(document.createElement("div"), {
                 children: [{
                     tag: 'span',
                     children: 'Span'
@@ -144,7 +144,7 @@ describe('createElement', function () {
     })
     
     it('array with element', function () {
-        assert.equal(assignAttributes(document.createElement("div"), {
+        assert.equal(setAttributes(document.createElement("div"), {
             children: [createElement('span', {
                 children: 'Span'
             })]
@@ -154,7 +154,7 @@ describe('createElement', function () {
   
 describe('content alias to children', function () {
     it('text', function () {
-        assert.equal(assignAttributes(document.createElement("div"), {
+        assert.equal(setAttributes(document.createElement("div"), {
             content: 'Hello World'
         }).outerHTML, `<div>Hello World</div>`);
     })
