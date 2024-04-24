efe7bfda6398ad31eabd62f5e71fdeefc344fad4
Ben Ehmke <benehmke@gmail.com>
2024-04-24 16:08:02 -0500

    Fix prepend not prepending when array or splat of args

--- test/prependTest.js
+++ test/prependTest.js
@@ -10,4 +10,18 @@ describe('prepend', function () {
     assert.equal(el.outerHTML, `<div id="a"><div id="c"></div><div id="b"></div></div>`)
   });
   
+  it('array element', function () {
+    const el = createElement('div', {id: 'a'})
+    prepend(el, createElement('div', {id: 'b'}))
+    prepend(el, [createElement('div', {id: 'c'}), createElement('div', {id: 'd'})])
+    assert.equal(el.outerHTML, `<div id="a"><div id="c"></div><div id="d"></div><div id="b"></div></div>`)
+  });
+  
+  it('splat elements', function () {
+    const el = createElement('div', {id: 'a'})
+    prepend(el, createElement('div', {id: 'b'}))
+    prepend(el, createElement('div', {id: 'c'}), createElement('div', {id: 'd'}))
+    assert.equal(el.outerHTML, `<div id="a"><div id="c"></div><div id="d"></div><div id="b"></div></div>`)
+  });
+  
 });
